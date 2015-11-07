e68771861700f3913edd1db36f8222a30377e690
marpy <gh19900922@gmail.com>
2015-11-07 23:13:13 +0800

    scroll modify Signed-off-by: marpy <gh19900922@gmail.com>

--- web/src/main/webapp/static/js/component/scrollspy.js
+++ web/src/main/webapp/static/js/component/scrollspy.js
@@ -1,7 +1,7 @@
 /**
  * 屏幕滚动时触发效果的控件
  */
-define(function(require, exports, module){
+define(function(require, exports, module) {
 	require('jquery');
 
 	module.exports = scrollspy;
@@ -9,47 +9,53 @@ define(function(require, exports, module){
 	function scrollspy(nodes) {
 		return new Scrollspy(nodes);
 	}
-	
-	function Scrollspy(nodes){
-		var list=[];
-		var self=this;
-		this._scroll=null;
-		$(nodes).each(function(){
-			var id=$(this).attr("id");
-			var height=$(this).offset().top;
-			var obj={
+
+	function Scrollspy(nodes) {
+		var list = [];
+		var self = this;
+		this._scroll = null;
+		$(nodes).each(function() {
+			var id = $(this).attr("id");
+			var height = $(this).offset().top;
+			var obj = {
 				id : id,
 				height : height
 			};
 			list.push(obj);
 		});
-		
-		list.sort(function(a, b){
+
+		list.sort(function(a, b) {
 			return a.height - b.height;
 		});
-		
+
 		this.scroll = function(callback) {
-			if(callback==undefined)
+			if (callback == undefined)
 				onScroll();
 			else
 				self._scroll = callback;
 			return self;
 		}
-		
+
 		$(window).scroll(onScroll);
-		
-		function onScroll(){
+
+		function onScroll() {
 			var top = $(document).scrollTop();
-			for(var i=list.length-1; i>=0; i--){
-				var d = list[i].height - top;
-				if(d<=0 || i==0){
-					if(self._scroll!=null)
-						self._scroll(list[i].id);
+			var winHeight = $(window).height();
+			var docHeight = $(document).height();
+			if (winHeight >= docHeight) {
+				self._scroll(list[0].id);
+				return;
+			}
+			var shift = top * docHeight / (docHeight - winHeight);
+			for (var i = 1; i <= list.length; ++i) {
+				if (i == list.length || shift < list[i].height) {
+					if (self._scroll != null)
+						self._scroll(list[i - 1].id);
 					break;
 				}
 			}
 		}
-		
+
 	}
 
 });
